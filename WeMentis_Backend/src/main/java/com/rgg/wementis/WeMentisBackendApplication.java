5b341b82d78dafe7cf4111704241d449b7c1e1ad
Rubén García <rubentxogg@hotmail.com>
2022-02-16 09:21:55 +0100

    [ADD] Project scaffolding

---
+++ WeMentis_Backend/src/main/java/com/rgg/wementis/WeMentisBackendApplication.java
@@ -0,0 +1,13 @@
+package com.rgg.wementis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeMentisBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeMentisBackendApplication.class, args);
+	}
+
+}
