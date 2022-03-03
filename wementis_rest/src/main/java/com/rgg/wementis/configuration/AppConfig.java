262c3407c90ef07fde9f195ae086137c3ac11e6c
rubentxogg <rubentxogg@hotmail.com>
2022-03-03 18:58:18 +0100

    [ADD] wementis_rest - Spring Security

---
+++ wementis_rest/src/main/java/com/rgg/wementis/configuration/AppConfig.java
@@ -0,0 +1,17 @@
+package com.rgg.wementis.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+@ComponentScan("com")
+public class AppConfig {
+	
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+}
