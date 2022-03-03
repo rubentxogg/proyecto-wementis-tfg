89ad2f26c6197cb867b619184144d0c1036e545f
rubentxogg <rubentxogg@hotmail.com>
2022-03-03 20:51:22 +0100

    [ADD] Configuración Spring Security

--- wementis_front/src/App.vue
+++ wementis_front/src/App.vue
@@ -5,6 +5,24 @@
       <router-link to="/about">About</router-link>
     </div>
     <router-view />
+
+    <form action="http://localhost:8080/wementis/login" method="POST">
+      <table>
+        <tr>
+          <td>User:</td>
+          <td><input type="text" name="username" value="" /></td>
+        </tr>
+        <tr>
+          <td>Password:</td>
+          <td><input type="password" name="password" /></td>
+        </tr>
+        <tr>
+          <td><input name="submit" type="submit" value="submit" /></td>
+        </tr>
+      </table>
+    </form>
+
+    <a href="http://localhost:8080/wementis/logout">LOGOUT</a>
   </div>
 </template>
 
--- wementis_rest/src/main/java/com/rgg/wementis/configuration/WementisSecurityConfiguration.java
+++ wementis_rest/src/main/java/com/rgg/wementis/configuration/WementisSecurityConfiguration.java
@@ -5,10 +5,12 @@ import javax.sql.DataSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.util.AntPathMatcher;
 
 @Configuration
 @EnableWebSecurity(debug = true)
@@ -28,5 +30,24 @@ public class WementisSecurityConfiguration extends WebSecurityConfigurerAdapter 
 	@Override
 	public void configure(WebSecurity web) throws Exception {
 		web.ignoring().antMatchers("/resources**");
+	}
+	
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.authorizeRequests()
+				.antMatchers("/**").hasRole("USUARIO")
+				.and()
+			.formLogin()
+				.loginPage("http://localhost:8081/")
+				.loginProcessingUrl("/login")
+				.usernameParameter("username")
+				.passwordParameter("password")
+				.failureUrl("http://localhost:8081/?error=true")
+				.and()
+			.logout()
+				.logoutUrl("/logout")
+				.deleteCookies("JSESSIONID");
+		
+		http.csrf().disable();
 	}
 }
