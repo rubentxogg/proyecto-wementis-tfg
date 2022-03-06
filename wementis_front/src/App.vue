442d4d4a8e15152ce3311d656656de7649874bcc
rubentxogg <rubentxogg@hotmail.com>
2022-03-06 13:37:52 +0100

    [ADD] wementis_front - Menú lateral terminado

--- wementis_front/src/App.vue
+++ wementis_front/src/App.vue
@@ -1,17 +1,31 @@
 <template>
-  <div class="app">
-    <side-nav-menu v-if="this.$route.name !== 'Home'"/>
-    <router-view />
+  <div class="app container-fluid">
+    <div class="row">
+      <side-nav-menu v-if="this.$route.name !== 'Home'" class="col-3" />
+      <router-view class="col bg-standard" />
+    </div>
   </div>
 </template>
 
 <script>
-import SideNavMenu from '@/components/SideNavMenu.vue';
+import SideNavMenu from "@/components/SideNavMenu.vue";
 
 export default {
   name: "App",
   components: {
-    SideNavMenu
-  }
+    SideNavMenu,
+  },
 };
 </script>
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Ubuntu:wght@300;400;500&display=swap');
+
+* {
+  font-family: 'Ubuntu', sans-serif;
+}
+
+.bg-standard {
+  background: rgb(255, 251, 251)
+}
+</style>
--- wementis_front/src/components/SideNavMenu.vue
+++ wementis_front/src/components/SideNavMenu.vue
@@ -1,14 +1,41 @@
 <template>
-  <aside class="sideNavMenu">
-      <h1>sideNavMenu</h1>
-      <router-link to="/dashboard">Dashboard</router-link>
-      <router-link to="/pacientes">Pacientes</router-link>
-      <router-link to="/psicologos">Psicólogos</router-link>
-      <router-link to="/tarifas">Tarifas</router-link>
-      <router-link to="/citas">Citas</router-link>
-      <router-link to="/informe">Informe</router-link>
+  <aside class="sideNavMenu border-end border-2">
+    <nav class="d-flex flex-column">
+      <div class="header d-flex justify-content-center align-items-center mt-3">
+        <img src="../assets/logo.png" width="70" height="70" alt="logo">
+        <h2 class="mt-3 text-primary">WeMentis</h2>
+      </div>
+      <hr>
 
-      <a href="http://localhost:8080/wementis/logout">LOGOUT</a>
+      <router-link to="/dashboard" class="p-2 text-dark rounded">
+        <i class="bi bi-speedometer2 me-2"></i>Dashboard
+      </router-link>
+
+      <router-link to="/pacientes" class="p-2 text-dark rounded">
+        <i class="bi bi-people me-2"></i>Pacientes
+      </router-link>
+
+      <router-link to="/psicologos" class="p-2 text-dark rounded">
+        <i class="bi bi-bandaid me-2"></i>Psicólogos
+      </router-link>
+
+      <router-link to="/tarifas" class="p-2 text-dark rounded">
+        <i class="bi bi-wallet2 me-2"></i>Tarifas
+      </router-link>
+
+      <router-link to="/citas" class="p-2 text-dark rounded">
+        <i class="bi bi-calendar-week me-2"></i>Citas
+      </router-link>
+
+      <router-link to="/informe" class="p-2 text-dark rounded">
+        <i class="bi bi-file-earmark-pdf me-2"></i>Informe
+      </router-link>
+
+      <hr>
+      <a href="http://localhost:8080/wementis/logout" class="text-center rounded">LOGOUT
+        <i class="bi bi-caret-down"></i>
+      </a>
+    </nav>
   </aside>
 </template>
 
@@ -19,4 +46,43 @@ export default {
 </script>
 
 <style scoped>
+.sideNavMenu {
+  height: 100vh;
+  width: 20rem;
+  position: relative;
+}
+
+.header{
+  margin-left: -1rem;
+  cursor: default;
+}
+
+hr{
+  height: 0.1rem !important;
+}
+
+nav{
+  position: fixed;
+  width: 18.3rem;
+}
+
+a {
+  text-decoration: none;
+  width: 100%;
+}
+
+a.router-link-exact-active {
+  color: #ffffff !important;
+  background-color: #0d6efd;
+  
+}
+
+a:hover {
+  color: #ffffff !important;
+  background-color: #00000041;
+}
+
+i {
+  margin-left: 4.5rem;
+}
 </style>
