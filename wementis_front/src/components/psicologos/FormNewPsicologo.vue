a1207a3472c36655236f21e400a592bac7cea676
rubentxogg <rubentxogg@hotmail.com>
2022-03-13 12:48:15 +0100

    [ADD] wementis_front - FormNewPsicologo validation

--- wementis_front/src/components/psicologos/FormNewPsicologo.vue
+++ wementis_front/src/components/psicologos/FormNewPsicologo.vue
@@ -2,27 +2,35 @@
    <form class="form-new-psicologo d-flex flex-wrap">
     <div class="input-group mb-3">
      <span class="input-group-text">Nombre</span>
-     <input type="text" class="form-control" name="nombre" v-model="nombre">
+     <input type="text" class="form-control" name="nombre" v-model="nombre" maxlength="25" placeholder="Ej. Rubén">
+     <i v-if="isCorrectNombre" class="bi bi-check2-circle fs-3 text-success"></i>
+     <i v-else class="bi bi-backspace fs-3 text-danger"></i>
     </div>
 
     <div class="input-group mb-3">
      <span class="input-group-text">Apellidos</span>
-     <input type="text" class="form-control" name="apellidos" v-model="apellidos">
+     <input type="text" class="form-control" name="apellidos" v-model="apellidos" maxlength="25" placeholder="Ej. García">
+     <i v-if="isCorrectApellidos" class="bi bi-check2-circle fs-3 text-success"></i>
+     <i v-else class="bi bi-backspace fs-3 text-danger"></i>
     </div>
 
     <div class="input-group mb-3">
      <span class="input-group-text">Teléfono</span>
-     <input type="text" class="form-control" name="telefono" v-model="telefono">
+     <input type="tel" class="form-control" name="telefono" v-model="telefono" maxlength="9" placeholder="9 Dígitos">
+     <i v-if="isCorrectTelefono" class="bi bi-check2-circle fs-3 text-success"></i>
+     <i v-else class="bi bi-backspace fs-3 text-danger"></i>
     </div>
 
     <div class="input-group mb-3">
      <span class="input-group-text">Email</span>
-     <input type="text" class="form-control" name="email" v-model="email">
+     <input type="email" class="form-control" name="email" v-model="email" placeholder="example@hotmail.com">
+     <i v-if="isCorrectEmail" class="bi bi-check2-circle fs-3 text-success"></i>
+     <i v-else class="bi bi-backspace fs-3 text-danger"></i>
     </div>
 
      <div class="modal-footer w-100">
         <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancelar</button>
-        <button type="button" class="btn btn-success" data-bs-dismiss="modal" @click="anadirPsicologo">Guardar</button>
+        <button type="button" :class="isFormValidationCorrect" data-bs-dismiss="modal" @click="anadirPsicologo">Guardar</button>
      </div>
   </form>
 </template>
@@ -39,6 +47,10 @@ export default {
       apellidos: "",
       telefono: "",
       email: "",
+      regNombre: /^[\w|á|é|í|ó|ú]+[\s]?[\w|á|é|í|ó|ú]*[\s]?[\w|á|é|í|ó|ú]*$/, // Juan José
+      regNumeros: /\d+/,
+      regTelefono: /^[\d]{9}$/,
+      regEmail: /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/
     };
   },
   methods: {
@@ -56,10 +68,35 @@ export default {
         .then(() => this.$emit("updateTabla"))
         .catch((err) => console.error(err));
     }
+  },
+  computed: {
+    isCorrectNombre() {
+      if(this.nombre === "" || !this.regNombre.test(this.nombre) || this.regNumeros.test(this.nombre)) return false;
+      return true;
+    },
+    isCorrectApellidos() {
+      if(this.apellidos === "" || !this.regNombre.test(this.apellidos) || this.regNumeros.test(this.apellidos)) return false;
+      return true;
+    },
+    isCorrectTelefono() {
+      if(this.telefono === "" || !this.regTelefono.test(this.telefono)) return false;
+      return true;
+    },
+    isCorrectEmail() {
+      if(this.email === "" || !this.regEmail.test(this.email)) return false;
+      return true;
+    },
+    isFormValidationCorrect() {
+      if(this.isCorrectNombre && this.isCorrectApellidos && this.isCorrectTelefono && this.isCorrectEmail) return "btn btn-success";
+      return "btn btn-outline-success disabled";
+    }
   }
 }
 </script>
 
-<style>
-
+<style scoped>
+div i{
+  position: absolute;
+  right: 0.5rem;
+}
 </style>
